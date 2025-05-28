6b1b0e37a5cae839a7853c1320686a0885c958f0
Jaco32 <jacek.wegorkiewicz@gmail.com>
2025-05-28 12:48:13 +0200

    use navigate for routing

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,12 +1,14 @@
 import '../css/SignUp.css';
 import '../App.css';
+import { useNavigate } from "react-router-dom";
 
 function SignUp() {
+    const navigate = useNavigate();
 
     return (
         <div className='App'>
             <button className="button-54" type='button'>Sing In</button><br/><br/>
-            <button className="button-54" type='button' onClick={(event) => window.location.assign('/set_login')}>Sing Up</button>
+            <button className="button-54" type='button' onClick={(event) => navigate('/set_login')}>Sing Up</button>
         </div>
     )
 }
