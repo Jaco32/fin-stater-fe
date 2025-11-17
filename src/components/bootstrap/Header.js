bab5003d795dff5de5d2e8602197fbff0ba9273e
Jaco32 <jacek.wegorkiewicz@gmail.com>
2025-11-17 22:07:29 +0100

    re-enable files adding in header in lame way

--- src/components/bootstrap/Header.js
+++ src/components/bootstrap/Header.js
@@ -1,12 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import { InputGroup } from "react-bootstrap";
 
 function Header() {
+    console.log("Rendering - Header")
+
+    const navigate = useNavigate();
 
     let sign_up_flag = true;
     let sign_in_flag = true;
     let log_out_flag = false;
+    let add_file_flag = false;
     
     if ((window.location.pathname === "/sign_up") || 
         (window.location.pathname === "/upload_transactions") || 
@@ -20,6 +27,26 @@ function Header() {
         log_out_flag = !log_out_flag;
     }
 
+    if (window.location.pathname === "/stats") add_file_flag = !add_file_flag;
+
+    function handleFileUpload(event) {
+        const file = event.target.files[0];
+        let myPromise = file.arrayBuffer();
+        myPromise.then(
+            function(value) {
+                var xhr = new XMLHttpRequest();
+                xhr.open("POST", process.env.REACT_APP_BACKEND_URL + '/transaction/add/', false);
+                xhr.setRequestHeader('mode', 'no-cors');
+                if (file.name.split(".")[1] === "xlsx") {
+                    xhr.setRequestHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+                } else {
+                    xhr.setRequestHeader('Content-Type', 'text/csv');
+                }
+                xhr.send(value);
+                navigate("/stats")
+        });
+    }
+
     return (
         <>
             <Navbar className="navbar-dark bg-primary">
@@ -27,6 +54,13 @@ function Header() {
                     <Navbar.Brand href="/">FinStat</Navbar.Brand>
                     <Navbar.Text>Basic Financial Statistics</Navbar.Text>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    {add_file_flag && (
+                        <Form inline>
+                            <InputGroup>
+                                <Form.Control type="file" onChange={handleFileUpload}/>
+                            </InputGroup>
+                        </Form>
+                    )}
                     <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end me-2">
                         <Nav>
                             {sign_in_flag && (<Nav.Link href="/sign_in">Sign In</Nav.Link>)}
